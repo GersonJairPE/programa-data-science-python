f416d42dbb20592cebca45183d635993034e0dd4
Gerson Pereyra <gersonjpe.94@hotmail.com>
2022-09-16 10:37:03 -0500

    first commit

---
+++ asdasd.py
@@ -0,0 +1,19 @@
+dic = {
+    "customer1":234,
+    "customer2":223,
+    "customer3":479,
+    "customer4":261,
+    "customer5":315,
+    "customer6":397,
+    "customer7":475,
+    "customer8":198,
+    "customer9":458,
+    "customer10":678,
+    "customer11":123,
+    "customer12":423,
+    "customer13":203,
+}
+
+print(type(dic))
+
+
